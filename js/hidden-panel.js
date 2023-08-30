556a9768fdf931516694061006227f99f2a96086
Slav-0N <SOVILGO@gmail.com>
2023-08-30 23:18:32 +0300

    дизайн и движок сделал

---
+++ js/hidden-panel.js
@@ -0,0 +1,12 @@
+const mainButton = document.querySelector('#messengers-float-button')
+const hiddenClass = document.querySelector('#messengers-panel-content')
+
+
+mainButton.addEventListener("click", cuca);
+
+
+function cuca(event) {
+  console.log('hello!');
+  hiddenClass.classList.toggle("hidden-panel")
+  mainButton.classList.toggle("rotatering-btn")
+}
